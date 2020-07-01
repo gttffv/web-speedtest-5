538ac28cf8f857d631904544b93296e12a0a365b
gttffv <66701766+gttffv@users.noreply.github.com>
2020-07-01 16:58:50 +0300

    تم حل التمرين

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,11 +1,11 @@
 let instructor = {
-  name: "Menawer",
+  name: "ahmed",
   courses: ["JavaScript", "React", "PHP"],
   githubAccount: {
     username: "careless10",
     numberOfRepos: 200,
   },
   sayHello: function () {
-    console.log("Hi, I'm Menawer");
+    console.log("Hi, I'm ahmed");
   },
 };
